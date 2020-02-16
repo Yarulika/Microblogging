21248b5142a5f688210fe23b8b96f517f2a8f89e
Anna Yarulina <anna.yarulina@gmail.com>
2020-02-16 13:24:32 +0200

    Initialized Spring Boot Rest API, added configuration file with required settings

---
+++ src/main/java/com/sda/microblogging/MicrobloggingApplication.java
@@ -0,0 +1,13 @@
+package com.sda.microblogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrobloggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrobloggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda/microblogging/MicrobloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.microblogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrobloggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
