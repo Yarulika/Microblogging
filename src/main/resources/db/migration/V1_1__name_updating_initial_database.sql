4dd6d448bd1b4ff46646111bebb939594cf976fd
MuhametSmaili <muhametsmaili@gmail.com>
2020-02-18 22:25:05 +0200

    Refactored some column names

--- src/main/resources/db/migration/V1_1__name_updating_initial_database.sql
+++ src/main/resources/db/migration/V1_1__name_updating_initial_database.sql
@@ -2,8 +2,22 @@ ALTER TABLE users CHANGE `privacy` `is_private` int;
 
 ALTER TABLE users CHANGE `account_status` `is_blocked` int;
 
-ALTER TABLE users CHANGE `user_role` `is_admin` int;
+ALTER TABLE users CHANGE `user_role` `role` int;
+
+ALTER TABLE user_role CHANGE `name` `title` varchar(20);
 
 ALTER TABLE followers add column id int PRIMARY KEY;
 
 ALTER TABLE followers CHANGE following_user_id follower_user_id int;
+
+ALTER TABLE comments ADD COLUMN content varchar(160);
+
+ALTER TABLE comment_likes CHANGE `creation_date` `liked_date` DATETIME;
+
+ALTER TABLE post_likes CHANGE `creation_date` `liked_date` DATETIME;
+
+ALTER TABLE posts CHANGE contents content  varchar(160);
+
+ALTER TABLE comment_likes ADD COLUMN id int primary key  auto_increment;
+
+ALTER TABLE post_likes ADD COLUMN id int primary key  auto_increment;
