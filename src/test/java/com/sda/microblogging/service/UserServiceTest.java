97e27b3d92a999affeeb84c5142034301ced1495
Anna Yarulina <anna.yarulina@gmail.com>
2020-02-22 19:47:56 +0200

    Brushing up UserServiceTest

--- src/test/java/com/sda/microblogging/service/UserServiceTest.java
+++ src/test/java/com/sda/microblogging/service/UserServiceTest.java
@@ -4,7 +4,7 @@ import com.sda.microblogging.common.RoleTitle;
 import com.sda.microblogging.entity.Role;
 import com.sda.microblogging.entity.User;
 import com.sda.microblogging.repository.UserRepository;
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -27,49 +27,45 @@ public class UserServiceTest {
     @InjectMocks
     private UserService userService;
 
-    @Test
-    public void save_returns_saved_user() throws Exception{
-        Role role = new Role();
-        role.setId(1);
-        role.setTitle(RoleTitle.ADMIN);
+    private User expectedUser;
 
-        User expectedUser = new User();
+    @BeforeEach
+    public void init(){
+        expectedUser = new User();
         expectedUser.setUserId(1);
         expectedUser.setUsername("name");
         expectedUser.setPassword("password");
         expectedUser.setEmail("name@mail.com");
         expectedUser.setPrivate(false);
         expectedUser.setAvatar("avatar");
+        expectedUser.setBlocked(false);
         expectedUser.setCreationDate(Date.valueOf("2020-01-01"));
+        Role role = new Role(1, RoleTitle.ADMIN);
         expectedUser.setRole(role);
+        expectedUser.setBlockedUsers(null);
+        expectedUser.setFollowers(null);
+    }
 
+    @Test
+    public void save_returns_saved_user(){
         when(userRepository.save(any(User.class))).thenReturn(expectedUser);
         User actualUser = userService.save(expectedUser);
-
-        assertEquals(expectedUser.getUsername(), actualUser.getUsername());
-        assertEquals(expectedUser.getEmail(), actualUser.getEmail());
+        assertEquals(expectedUser, actualUser);
     }
 
     @Test
     public void findUserByUsername_with_given_correct_username_OK() {
-        String testUsername = "testUsername";
-        User expectedUser = new User(null, "testUsername", "password", "email", true, null, false, null, null, null, null);
+        String testUsername = expectedUser.getUsername();
         when(userRepository.findByUsername(testUsername)).thenReturn(expectedUser);
-
         User actualUser = userService.findUserByUsername(testUsername).get();
         assertEquals(expectedUser.getUsername(), actualUser.getUsername());
     }
 
     @Test
-    public void getById_existing_user_OK() throws Exception{
-        int testId = 100;
-        User expectedUser = new User(testId, "testUsername", "password", "email", true, null, false, null, null, null, null);
-
+    public void getById_with_given_correct_id_OK() throws Exception{
+        int testId = expectedUser.getUserId();
         when(userRepository.findById(testId)).thenReturn(Optional.of(expectedUser));
         User actualUser = userService.getById(testId).orElseThrow(Exception::new);
-
-        assertEquals(expectedUser.getUsername(), actualUser.getUsername());
-        assertEquals(expectedUser.getEmail(), actualUser.getEmail());
+        assertEquals(expectedUser, actualUser);
     }
-
 }
