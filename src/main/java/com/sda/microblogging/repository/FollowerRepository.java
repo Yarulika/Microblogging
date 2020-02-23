61864caf00274d230183c0c551246d1e7a9f2c29
Bikram Karki <himalayanepalinsis@gmail.com>
2020-02-23 18:07:54 +0200

    finalized follower service

--- src/main/java/com/sda/microblogging/repository/FollowerRepository.java
+++ src/main/java/com/sda/microblogging/repository/FollowerRepository.java
@@ -4,6 +4,17 @@ import com.sda.microblogging.entity.Follower;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface FollowerRepository extends JpaRepository<Follower, Integer> {
+    List<Follower> findAllFollowersByUser(Integer userId);
+
+    List<Follower> findAllFollowingByFollower(Integer followerId);
+
+    Optional<Follower> findFollowerByUserAndFollower(Integer userId, Integer followerId);
+
+    void deleteByUserAndFollower(Integer userId, Integer followerId);
 }
+
