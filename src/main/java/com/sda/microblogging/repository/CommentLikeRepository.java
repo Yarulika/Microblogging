a12f811bb208619429a42a6eb73239f636ec6209
Muhamet Smaili <muhametsmaili@gmail.com>
2020-02-26 20:39:33 +0200

    added comment_like_service with TDD, included the method i think we need

--- src/main/java/com/sda/microblogging/repository/CommentLikeRepository.java
+++ src/main/java/com/sda/microblogging/repository/CommentLikeRepository.java
@@ -1,9 +1,15 @@
 package com.sda.microblogging.repository;
 
+import com.sda.microblogging.entity.Comment;
 import com.sda.microblogging.entity.CommentLike;
+import com.sda.microblogging.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CommentLikeRepository extends JpaRepository<CommentLike, Integer> {
+    Optional<CommentLike> findByCommentAndUser(Comment comment, User user);
+    int countByComment(int commentId);
 }
---
+++ src/main/java/com/sda/microblogging/service/CommentLikeService.java
@@ -0,0 +1,29 @@
+package com.sda.microblogging.service;
+
+import com.sda.microblogging.entity.Comment;
+import com.sda.microblogging.entity.CommentLike;
+import com.sda.microblogging.entity.User;
+import com.sda.microblogging.repository.CommentLikeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.validation.constraints.NotNull;
+
+@Service
+public class CommentLikeService {
+    @Autowired
+    CommentLikeRepository commentLikeRepository;
+
+
+    public void toggleCommentLike(@NotNull CommentLike commentLike) {
+        if (!commentLikeRepository.findByCommentAndUser(commentLike.getComment(),commentLike.getUser()).isPresent()){
+            commentLikeRepository.save(commentLike);
+        }else
+            commentLikeRepository.delete(commentLike);
+    }
+
+
+    public int getNumberOfCommentLikes(@NotNull int commentId){
+        return commentLikeRepository.countByComment(commentId);
+    }
+}
