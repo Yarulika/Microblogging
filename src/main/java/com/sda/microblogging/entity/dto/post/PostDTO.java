93e0d27ef8716fdb8a54d41c5051db97f13c00bd
Muhamet Smaili <muhametsmaili@gmail.com>
2020-02-29 10:41:38 +0200

    added the post_controller just one method

---
+++ src/main/java/com/sda/microblogging/entity/dto/post/PostDTO.java
@@ -0,0 +1,33 @@
+package com.sda.microblogging.entity.DTO.post;
+
+import com.sda.microblogging.common.RoleTitle;
+import com.sda.microblogging.service.PostLikeService;
+import com.sda.microblogging.service.PostService;
+import com.sda.microblogging.service.UserService;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.mapstruct.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.sql.Date;
+import java.util.List;
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+public class PostDTO {
+    private String username;
+    private int userId;
+    private boolean isUserPrivate;
+    private RoleTitle userRole;
+    private String avatar;
+    private int numberOfPostShares;
+    private int postId;
+    private String content;
+    private boolean isPostEdited;
+    private Date postCreatedDate;
+    private int numberOfPostLikes;
+    private int numberOfComments;
+}
---
+++ src/main/java/com/sda/microblogging/entity/mapper/postMapperTest.java
@@ -0,0 +1,24 @@
+package com.sda.microblogging.entity.Mapper;
+
+import com.sda.microblogging.entity.DTO.post.PostDTO;
+import com.sda.microblogging.entity.Post;
+import com.sda.microblogging.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class postMapperTest extends PostDTO {
+
+    @Autowired
+    private Post post;
+    @Autowired
+    private User user;
+    @Autowired
+    private int nrOfPostLikes;
+
+    postMapperTest(Post post, User user, int nrOfLikes){
+        this.post=post;
+        this.user=user;
+        this.nrOfPostLikes=nrOfLikes;
+    }
+
+
+}
