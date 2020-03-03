580893d3e712c4de8de950a0cad909458a5e2b76
Anna Yarulina <anna.yarulina@gmail.com>
2020-03-03 20:50:14 +0200

    Basic DTOs and mapper for comment

---
+++ src/main/java/com/sda/microblogging/entity/DTO/comment/CommentDTO.java
@@ -0,0 +1,21 @@
+package com.sda.microblogging.entity.DTO.comment;
+
+import lombok.*;
+
+import java.sql.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CommentDTO {
+    private int id;
+    private String content;
+    private int postId;
+    private int userOwnerId;
+    private String userOwnerUsername;
+    private Date creationDate;
+    private Integer commentParentId;
+    private int numberOfCommentLikes;
+}
