248413ff83610fa12143afa2e2356271b77c591e
Anna Yarulina <anna.yarulina@gmail.com>
2020-03-03 20:51:21 +0200

    Comment controller modified to use DTOs

--- src/main/java/com/sda/microblogging/controller/CommentController.java
+++ src/main/java/com/sda/microblogging/controller/CommentController.java
@@ -1,50 +1,60 @@
 package com.sda.microblogging.controller;
 
 import com.sda.microblogging.entity.Comment;
-import com.sda.microblogging.entity.Follower;
+import com.sda.microblogging.entity.DTO.comment.CommentDTO;
+import com.sda.microblogging.entity.DTO.comment.CommentSavedDTO;
+import com.sda.microblogging.entity.mapper.CommentDTOMapper;
 import com.sda.microblogging.service.CommentService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/microblogging/v1")
 public class CommentController {
 
     private final CommentService commentService;
+    private final CommentDTOMapper commentDtoMapper;
 
     @Autowired
-    public CommentController(CommentService commentService){
+    public CommentController(CommentService commentService, CommentDTOMapper commentDtoMapper){
         this.commentService = commentService;
+        this.commentDtoMapper = commentDtoMapper;
     }
 
     @ApiOperation(value = "Add comment", notes = "Add new comment")
     @PostMapping(path = "/comment")
     @ResponseBody
-    public ResponseEntity<Comment> saveNew(@Valid @RequestBody Comment comment){
-        return new ResponseEntity<>(commentService.save(comment), HttpStatus.CREATED);
+    public ResponseEntity<CommentSavedDTO> saveNew(@Valid @RequestBody Comment comment){
+        Comment commentSaved = commentService.save(comment);
+        return new ResponseEntity<>(commentDtoMapper.toCommentSavedDto(commentSaved), HttpStatus.CREATED);
     }
 
     @ApiOperation(value = "Find comments for post", notes = "Find all comments for given post id")
     @GetMapping(path = "/post/{postId}/comment")
     @ResponseStatus(HttpStatus.FOUND)
     @ResponseBody
-    public Iterable<Comment> findCommentsByPostId(@PathVariable @NotBlank int postId){
-        return commentService.findCommentsByPostId(postId);
+    public Iterable<CommentDTO> findCommentsByPostId(@PathVariable @NotBlank int postId){
+        return commentService.findCommentsByPostId(postId)
+                .parallelStream()
+                .map(commentDtoMapper::toCommentDto)
+                .collect(Collectors.toList());
     }
 
     @ApiOperation(value = "Find sub comments for parent comment", notes = "Find all kids comments for given parent comment id")
     @GetMapping(path = "/comment/{commentParentId}")
     @ResponseStatus(HttpStatus.FOUND)
     @ResponseBody
-    public Iterable<Comment> findCommentsByCommentParentId(@PathVariable @NotBlank int commentParentId){
-        return commentService.findCommentsByCommentParentId(commentParentId);
+    public Iterable<CommentDTO> findCommentsByCommentParentId(@PathVariable @NotBlank int commentParentId){
+        return commentService.findCommentsByCommentParentId(commentParentId)
+                .parallelStream()
+                .map(commentDtoMapper::toCommentDto)
+                .collect(Collectors.toList());
     }
 }
